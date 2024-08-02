3eebd002f0d51c309632593a1eeb7acdf60b3e60
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-02 18:29:12 +0530

    app file modified

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import dotenv from "dotenv"
 import DBConnect from "./db/dbconn.js"
+import app from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
-DBConnect();
+DBConnect() //this function is async use .then and catch for further handling
+.then(()=>{
+  console.log("MongoDB connnected Successfully !!!");
+  app.on("error",(error)=>{
+    console.error(`An error occured in starting server !!! `,error);
+  })
+
+  app.listen(process.env.PORT || 8000 ,()=>{
+    console.log(`SERVER listening to you at port ${process.env.PORT}`);
+  })
+})
+.catch(()=>{
+  console.error("MongoDB connection Failed : ",error);  
+}) 
+
 
 /*
 import express from "express";
