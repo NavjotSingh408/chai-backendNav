6e4df7eaaff2f887de9a2af29f302b1431232942
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-03 10:15:08 +0530

    configurations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from "express"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
 
 const app = express()
+app.use(cors({
+    origin : process.env.CORS_ORI,
+    credentials : true
+}))   //cors configuration
+
+app.use(express.json({limit : "16kb"})) // json limit configuration
+app.use(express.urlencoded({extended : true , limit : "16kb"}))  // url encoded config
+app.use(express.static('public'))
+app.use(express.cookieParser())
 
 export default app;
