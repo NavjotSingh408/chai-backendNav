034aec71dbfd06eb8879ec263d31d64d908303d3
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-05 12:27:07 +0530

    utility files added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+// const asyncHandler = (requestHandler) => {
+//     return (req,res,next) => {
+//         Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+//     }
+// } //handking with promise
+
+asyncHandler();
+
+const asyncHandler = (reqHandler) =>async (req,res,next) =>{
+    try {
+        await reqHandler(req,res,next)
+    } catch (error) {
+        res.staus(error.code).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
