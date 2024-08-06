056c00bdd158a43d04abfe8c9d66641feb9970bd
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-06 11:33:30 +0530

    multer and cloudinary config

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -57,4 +57,31 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return bcrypt.compare(password, this.password);
 }
 
+userSchema.methods.generateAccessToken() = function (){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username : this.username,
+            fullname : this.fullname,
+            email : this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken() = function (){
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
