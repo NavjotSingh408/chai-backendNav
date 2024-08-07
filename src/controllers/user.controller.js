bf5ec9da7666b4417ad1e75260df538af5c1c82f
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-07 17:47:55 +0530

    getting details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiErrors } from "../utils/ApiErrors.js"
+import { User } from "../models/user.modal.js"
+import { uploadToCloudnairy } from "../utils/cloudnairy.js"
 
 const registerUser = asyncHandler( async (req,res) => {
     // res.status(200).json({
@@ -14,6 +17,35 @@ const registerUser = asyncHandler( async (req,res) => {
     //create user object 
     //check response and remove pass and refresh token
     //return response
+
+    const {username,email,fullname,avatar,password} = req.body
+
+    if (
+        [username,email,fullname,avatar,password].some((field)=>field?.trim() === "")
+    ) {
+        throw new ApiErrors(400,"All fields are necessary")
+    }
+
+    const userExisted = User.findOne(
+        {
+            $or : [{ username },{ email }]
+        }
+    )    //finds first one that matches
+
+    if (userExisted) {
+        throw new ApiErrors(409, "User with same details already exists")
+    }
+    
+    const avatarLoacalPath = req.files?.avatar[0].path;
+    const coverLocalPath = req.files?.avatar[0].path;
+
+    if (!avatarLoacalPath) {
+        throw new ApiErrors(400,"Avatar not found")
+    }
+    
+    const avtarRef = uploadToCloudnairy(avatarLoacalPath)
+
+    
 })
 
 export {registerUser}
