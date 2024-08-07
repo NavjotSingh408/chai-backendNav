e02768f2441a257b796b5ac6a00fb6dda2878745
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-07 11:22:18 +0530

    added and tested route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
-import cookieParser from "cookie-parser";
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 
 const app = express()
@@ -12,6 +12,12 @@ app.use(cors({
 app.use(express.json({limit : "16kb"})) // json limit configuration
 app.use(express.urlencoded({extended : true , limit : "16kb"}))  // url encoded config
 app.use(express.static('public'))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+//import routes
+import router from "./routes/user.router.js"
+
+//routes declaration
+app.use("/api/v1/users" , router)
 
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,18 +1,18 @@
-// const asyncHandler = (requestHandler) => {
-//     return (req,res,next) => {
-//         Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
-//     }
-// } //handking with promise
-
-asyncHandler();
-
-const asyncHandler = (reqHandler) =>async (req,res,next) =>{
-    try {
-        await reqHandler(req,res,next)
-    } catch (error) {
-        res.staus(error.code).json({
-            success : false,
-            message : error.message
-        })
+const asyncHandler = (requestHandler) => {
+    return (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
-}
+} //handking with promise
+
+// const asyncHandler = (reqHandler) =>async (req,res,next) =>{
+//     try {
+//         await reqHandler(req,res,next)
+//     } catch (error) {
+//         res.staus(error.code).json({
+//             success : false,
+//             message : error.message
+//         })
+//     }
+// }
+
+export {asyncHandler}
