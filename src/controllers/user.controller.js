51ea89b2324b0435f6a8adcb2b31aa150cbaaa79
Navjot Singh Sidhu <singhsidhunavjot888@gmail.com>
2024-08-07 15:02:42 +0530

    route creation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message : "ok"
-    })
+    // res.status(200).json({
+    //     message : "ok yes mera phone hai Poco x6 pro 5g"
+    // })
+
+    //steps
+    //get user details
+    //validate - not empty
+    //check if already exist or not
+    //check for images , avatar
+    //upload them to cloudnairy , avatar
+    //create user object 
+    //check response and remove pass and refresh token
+    //return response
 })
 
 export {registerUser}
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"; //this upload will be used by us as a middleware
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([  //this is our middleware that will execute before registerUser
+        {
+            name: "avatar",
+            maxCount :1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),registerUser)
 
 export default router
